ba22472f79956b13522991e5101aaca056a10d51
Ariiq Yusuf <yusufariiq3@gmail.com>
2024-12-19 18:01:39 +0700

    add vercel config for deployment

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
 export default {
   content: [
     "./index.html",
@@ -24,7 +26,7 @@ export default {
       }
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
   daisyui: {
     themes: [],
     darkTheme: "dark",
